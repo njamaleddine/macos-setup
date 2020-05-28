20b24f2f2056a7c4768f13ed7a35f2f92a58af84
Nabil Jamaleddine <njamaleddine@users.noreply.github.com>
2020-05-27 22:01:42 -0400

    update packages, prompts and vscode settings

--- run.sh
+++ run.sh
@@ -5,9 +5,8 @@ brew_packages=(
   bash
   bash-completion
   bat
-  caskroom/cask/brew-cask
-  ffmpeg --with-faac --with-fdk-aac --with-libvorbis --with-ffplay --with-theora
-  fluid-synth --with-libsndfile
+  ffmpeg # --with-faac --with-fdk-aac --with-libvorbis --with-ffplay --with-theora
+  fluid-synth # --with-libsndfile
   fzf
   geos
   git
@@ -38,34 +37,37 @@ brew_packages=(
 
 # Brew Cask Applications
 brew_cask_apps=(
-  android-file-transfer
-  android-studio
-  arduino
-  docker-toolbox
-  eclipse-ide
-  etcher
+  docker
   firefox
   gimp
   google-chrome
   iterm2
-  java
-  ngrok
+  mongodb-compass
   postman
   robo-3t
-  sketch
-  sketchup
   skitch
-  skype
   slack
   spotify
-  sublime-text
   spectacle
-  virtualbox
   visual-studio-code
+)
+
+optional_cask_apps=(
+  android-file-transfer
+  android-studio
+  arduino
+  discord
+  etcher
+  ngrok
+  java
+  sketch
+  sketchup
+  skype
+  virtualbox
   vlc
 )
 
-USE_BASH_PROFILE=false
+REPLACE_ZSHRC_FILE=false
 USE_VSCODE_SETTINGS=false
 
 read -p "Specify any additional brew packages (separate each with a space or press ENTER to continue): " additional_packages
@@ -75,21 +77,39 @@ read -p "Specify any additional brew cask apps (separate each with a space or pr
 brew_cask_apps=("${brew_cask_apps[@]}" "${additional_cask_apps[@]}")
 
 while true; do
-    read -p "Replace ~/.bash_profile with new one? (y/n): " yn
-    case $yn in
-        [Yy]* ) USE_BASH_PROFILE=true; break;;
-        [Nn]* ) break;;
-        * ) echo "You must enter yes or no";;
-    esac
+  read -p "Install optional brew cask apps? (y/n): " yn
+  case $yn in
+  [Yy]*)
+    brew_cask_apps=("${brew_cask_apps[@]}" "${optional_cask_apps[@]}")
+    break
+    ;;
+  [Nn]*) break ;;
+  *) echo "You must enter yes or no" ;;
+  esac
 done
 
 while true; do
-    read -p "Replace visual studio code settings.json file with new one? (y/n): " yn
-    case $yn in
-        [Yy]* ) USE_VSCODE_SETTINGS=true; break;;
-        [Nn]* ) break;;
-        * ) echo "You must enter yes or no";;
-    esac
+  read -p "Replace ~/.zshrc with new one? (y/n): " yn
+  case $yn in
+  [Yy]*)
+    REPLACE_ZSHRC_FILE=true
+    break
+    ;;
+  [Nn]*) break ;;
+  *) echo "You must enter yes or no" ;;
+  esac
+done
+
+while true; do
+  read -p "Replace visual studio code settings.json file with new one? (y/n): " yn
+  case $yn in
+  [Yy]*)
+    USE_VSCODE_SETTINGS=true
+    break
+    ;;
+  [Nn]*) break ;;
+  *) echo "You must enter yes or no" ;;
+  esac
 done
 
 # Install Homebrew
@@ -106,26 +126,22 @@ echo "Cleaning up after installation..."
 brew cleanup
 
 echo "Installing nvm"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-if [ $USE_BASH_PROFILE ]; then
+if [ $REPLACE_ZSHRC_FILE ]; then
   echo "Setting bash profile..."
   cp .bash_profile ~/.bash_profile
 fi
 
 echo "Setting up Visual Studio Code..."
-bash ./vscode/setup.sh
+./vscode/setup.sh
 if [ $USE_VSCODE_SETTINGS ]; then
   echo "Overwriting visual-studio-code settings..."
   cp ./vscode/settings.json ~/Library/Application\ Support/Code/User/_settings-test.json
 fi
 
-echo "Installing python packages..."
-pip install --upgrade pip
-pip install ipython httpie pipenv virtualenvwrapper 
+echo "Brew cask services available to start:"
+brew services list
+echo "If you'd like to start services you can do so using the command: `brew services start ...`"
 
-echo "Starting services..."
-brew services start postgresql
-brew services start redis
-
-echo "MacOS setup complete!"
+echo "macOS setup complete!"
